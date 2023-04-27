854fdc476d20510ad81a3ee2d13158470a9b9d67
Matthew Aragon <test@test.com>
2023-04-27 16:36:24 -0700

    init commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchBar from './components/SearchBar';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
