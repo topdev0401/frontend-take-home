0251ad7f4471cb91afd1c0af879e66c2186b13ad
Matthew Aragon <test@test.com>
2023-04-27 18:14:04 -0700

    feature(package search): handle loading

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,19 +3,32 @@ import PackageList from "./components/PackageList";
 import SearchBar from "./components/SearchBar";
 import { IPackage } from "./utils/interfaces";
 import { getPackages } from "./api/packages";
+import { LinearProgress } from "@mui/material";
 
 import "./App.css";
 
 function App() {
   const [packages, setPackages] = useState<IPackage[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const fetchPackages = async (keyword: string): Promise<void> => {
+    setLoading(true);
+
+    if (!keyword) {
+      setPackages([]);
+      setLoading(false);
+      return;
+    }
+
     const apiResponse = await getPackages({ keyword });
     setPackages(apiResponse);
+
+    setLoading(false);
   };
 
   return (
     <div className="App">
+      {loading && <LinearProgress />}
       <SearchBar fetch={fetchPackages} />
       <PackageList packages={packages} />
     </div>
